5e954f77a85f1d7a3004037490dbc30ffddc7990
Mike Tikh <Mike@Mikes-MacBook-Pro.local>
2017-09-18 20:39:57 -0500

    working grid and color picker

--- designs.js
+++ designs.js
@@ -1,10 +1,55 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
+// Selectors for submit box, grid table, grid dimensions, color picker
+const submit = $('input[type=submit]');
+const gridTable = $('#pixel_canvas');
+const gHeight = $('#input_height');
+const gWidth = $('#input_width')
+const colorPicker = $('#colorPicker');
 
+// Fn: After uses clicks submit, creates grid of height and width
 function makeGrid() {
+  // Clears grid
+  gridTable.html('');
 
-// Your code goes here!
+  // Sets height and width from input boxes
+  let currentHeight = gHeight.val();
+  let currentWidth = gWidth.val();
 
+  // Row and Col iterators
+  let h = 1,
+      w = 1;
+
+  // Used to store the row number and fetch current row
+  let rowID, currentRow;
+
+  // for h in currentHeight, appends row
+  while (h <= currentHeight) {
+    rowID = 'row' + h;
+    // appends row with id = row[num]
+    gridTable.append(`<tr id="${rowID}"></tr>`);
+
+    // Gets current Row by ID to add cols
+    currentRow = $(`#${rowID}`);
+
+    while (w <= currentWidth) {
+      currentRow.append(`<td></td>`);
+      w++;
+    }
+
+    // Resets col to 1, goes to next Row
+    w = 1;
+    h++;
+  }
 }
+
+// After clicking submit, prevents refresh and runs grid
+submit.click(function(e) {
+  e.preventDefault();
+  makeGrid();
+});
+
+// If TD in table clicked, changes color to colorPicker value
+gridTable.on('click','td', function() {
+  currentColor = colorPicker.val();
+  $(this).css('background-color', currentColor);
+})
